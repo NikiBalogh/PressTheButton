deab98341f3cd0e38fe21b99f02bf1a298bf0ecc
Niki Franz Højbjerg Balogh <nikifranz@hotmail.dk>
2023-03-09 10:49:27 +0100

    Add project files.

---
+++ PressTheButton/Controller/Controller.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PressTheButton.Controller
+{
+    class Controller
+    {
+        int score = 0;
+        int x = 365;
+        int y = 180;
+        int moveSpeedX = 2;
+        int untilSpeedIncreaseX = 10;
+        bool moveX = false;
+        int moveSpeedY = 2;
+        int untilSpeedIncreaseY = 30;
+        bool moveY = false;
+
+        public int Score { get => score; set => score = value; }
+        public int X { get => x; set => x = value; }
+        public int Y { get => y; set => y = value; }
+
+        public void UpdateStuffAlways()
+        {
+            if (Score >= 5)
+            {
+                ButtonMoveXLogic();
+            }
+            if (Score >= 25)
+            {
+                ButtonMoveYLogic();
+            }
+            if (Score >= untilSpeedIncreaseX && Score <= 25)
+            {
+                untilSpeedIncreaseX += 5;
+                moveSpeedX += 2;
+            }
+            else if (Score >= untilSpeedIncreaseY && Score <= 50)
+            {
+                untilSpeedIncreaseY += 5;
+                moveSpeedY += 2;
+            }
+        }
+        private void ButtonMoveXLogic()
+        {
+            if (X <= 0)
+            {
+                moveX = false;
+            }
+            else if (X >= 700)
+            {
+                moveX = true;
+            }
+            if (!moveX)
+            {
+                MoveButtonRight(moveSpeedX);
+            }
+            else if (moveX)
+            {
+                MoveButtonLeft(moveSpeedX);
+            }
+        }
+
+        private void ButtonMoveYLogic()
+        {
+            if (Y <= 0)
+            {
+                moveY = false;
+            }
+            else if (Y >= 350)
+            {
+                moveY = true;
+            }
+            if (!moveY)
+            {
+                MoveButtonDown(moveSpeedY);
+            }
+            else if (moveY)
+            {
+                MoveButtonUp(moveSpeedY);
+            }
+        }
+
+        private void MoveButtonRight(int amount)
+        {
+            X += amount;
+        }
+
+        private void MoveButtonLeft(int amount)
+        {
+            X -= amount;
+        }
+
+        private void MoveButtonUp(int amount)
+        {
+            Y -= amount;
+        }
+
+        private void MoveButtonDown(int amount)
+        {
+            Y += amount;
+        }
+    }
+}
+
---
+++ PressTheButton/View/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+using PressTheButton.Controller;
+
+namespace PressTheButton
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Controller.Controller controller = new Controller.Controller();
+        TimeSpan AlwaysSpeed = new TimeSpan(10000);
+        DispatcherTimer timer = new DispatcherTimer();
+        public MainWindow()
+        {
+            InitializeComponent();
+            btnPressMe.Margin = new Thickness(controller.X, controller.Y, 0, 0);
+            timer.Tick += new EventHandler(Timer_Tick);
+            timer.Interval = AlwaysSpeed;
+            timer.Start();
+        }
+
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            lblScore.Content = controller.Score;
+            controller.UpdateStuffAlways();
+            btnPressMe.Margin = new Thickness(controller.X, controller.Y, 0, 0);
+            if (controller.Score >= 50)
+            {
+                gridGame.Visibility = Visibility.Collapsed;
+                gridWin.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void btnPressMe_Click(object sender, RoutedEventArgs e)
+        {
+            controller.Score++;
+        }
+
+    }
+}
